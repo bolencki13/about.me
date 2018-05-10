0dffddccf79f9eb827372d1baf47b67b8331e5f7
bolencki13 <bolencki13@bolencki13.com>
2018-05-10 14:52:20 -0400

    menu button

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -15,6 +15,9 @@ export default class Menu extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+              <li className="nav-item">
                 <a className="nav-link" href="#projects">Projects</a>
               </li>
               <li className="nav-item">
